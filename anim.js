ce986359e238a2056fb20ca55c1c7206c1ef43de
IvoCarrascoMediaAgency <40969344+IvoCarrascoMediaAgency@users.noreply.github.com>
2020-01-28 17:28:13 -0500

    first commit

---
+++ anim.js
@@ -0,0 +1,7 @@
+var animation_fondo_cohete_home = bodymovin.loadAnimation({
+    container: document.getElementById('animism'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'json/animism.json',  
+    })
